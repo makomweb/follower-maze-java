e1d6a931b65a56d1c051ce1ef62f54f4276e7f19
Martin Komischke <martin.komischke@outlook.com>
2019-04-08 19:42:34 +0200

    cleanup whitespaces

--- src/main/java/com/maze/EventQueueProcessor.java
+++ src/main/java/com/maze/EventQueueProcessor.java
@@ -4,33 +4,31 @@ import java.util.Queue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class EventQueueProcessor implements Runnable {
-    private final Users users;
-    private final Queue<Event> eventQueue;
-    private final AtomicBoolean wasCancelled;
-    int sequenceNumer = 1;
+	private final Users users;
+	private final Queue<Event> eventQueue;
+	private final AtomicBoolean wasCancelled;
+	int sequenceNumer = 1;
 
-    public EventQueueProcessor(Users users, Queue<Event> eventQueue, AtomicBoolean wasCancelled) {
-        this.users = users;
-        this.eventQueue = eventQueue;
-        this.wasCancelled = wasCancelled;
-    }
+	public EventQueueProcessor(Users users, Queue<Event> eventQueue, AtomicBoolean wasCancelled) {
+		this.users = users;
+		this.eventQueue = eventQueue;
+		this.wasCancelled = wasCancelled;
+	}
 
-    @Override
-    public void run() {
-        while (!wasCancelled.get()) {
-        	final Event peek = eventQueue.peek();
-            if (peek != null && peek.getSequenceNumber() <= sequenceNumer) {
-	            final Event event = eventQueue.poll();
-	            sequenceNumer++;
-	            
-	            if (event != null) {
-	                try {
-	                    event.raiseEvent(users);
-	                } catch (RuntimeException  ex) {
-	                    Logger.LogException("raiseEvent() has thrown", ex);
-	                }
-	            }
-	        }
-        }
-    }
+	@Override
+	public void run() {
+		while (!wasCancelled.get()) {
+			Event event = eventQueue.peek();
+			if (event != null && event.getSequenceNumber() <= sequenceNumer) {
+				event = eventQueue.poll();
+				sequenceNumer++;
+
+				try {
+					event.raiseEvent(users);
+				} catch (RuntimeException ex) {
+					Logger.LogException("raiseEvent() has thrown", ex);
+				}
+			}
+		}
+	}
 }
