3c09e19f21c3f541244e1d77e519b9f8e71ad45a
Martin Komischke <martin.komischke@outlook.com>
2019-05-04 11:15:26 +0200

    add user repository tests

--- src/test/java/com/maze/UserTests.java
+++ src/test/java/com/maze/UserTests.java
@@ -1,8 +1,10 @@
 package com.maze;
 
 import com.maze.events.FollowEvent;
+import com.maze.tools.NullStream;
 import com.maze.tools.PrintWriterFrom;
 import com.maze.users.User;
+import com.maze.users.UsersRepository;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -12,9 +14,9 @@ import static org.junit.Assert.*;
 
 public class UserTests {
     @Test
-    public void writing_event_should_succeed() {
+    public void Writing_event_should_succeed() {
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        PrintWriter writer = PrintWriterFrom.FromStream(byteStream);
+        PrintWriter writer = PrintWriterFrom.Stream(byteStream);
         User user = new User(1, writer);
         user.consumeEvent(new FollowEvent(44, 22, 11));
 
@@ -22,5 +24,20 @@ public class UserTests {
         String str = new String(bytes);
 
         assertEquals("44|F|22|11\r\n", str);
+    }
+
+    @Test
+    public void Adding_user_to_repo_should_succeed() {
+        UsersRepository repo = new UsersRepository();
+        repo.add(1, new NullStream());
+        User user = repo.get(1);
+        assertTrue(user.getId() == 1);
+    }
+
+    @Test
+    public void Fetching_from_empty_repo_should_fill_repo_and_give_user() {
+        UsersRepository repo = new UsersRepository();
+        User user = repo.get(44);
+        assertTrue(user.getId() == 44);
     }
 }
